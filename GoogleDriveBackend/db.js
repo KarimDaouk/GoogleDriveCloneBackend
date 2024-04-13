c74e3b33abc099ed84a7be8456f0797e7a4618f1
KarimDaouk <73751413+KarimDaouk@users.noreply.github.com>
2024-04-13 15:52:48 +0300

    Update db.js

--- GoogleDriveBackend/db.js
+++ GoogleDriveBackend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = 'mongodb://localhost:27017/GoogleDrive'; 
-const mongoURI ='mongodb+srv://KarimDaouk:Wd03405224@googledriveclone.trdr6wx.mongodb.net/GoogleDrive'
+const mongoURI ='mongodb+srv://KarimDaouk:Wd03405224@googledriveclone.trdr6wx.mongodb.net/GoogleDrive?retryWrites=true&w=majority'
 
 const connectDB = async () => {
   try {
